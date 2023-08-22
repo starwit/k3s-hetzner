46f57206ca2da25360a09b811439aeeeb9d3991d
flonix8 <flstanek@googlemail.com>
2023-08-22 09:20:22 +0200

    Add hcloud_firewall resource (allowing ICMP and SSH) and attach to server

--- hcloud.tf
+++ hcloud.tf
@@ -16,12 +16,38 @@ data "template_file" "cloud_init" {
 #  value = "${data.template_file.cloud_init.rendered}"
 #}
 
+# Allow only incoming ICMP and SSH
+resource "hcloud_firewall" "k3s-firewall" {
+  name = "k3s-firewall"
+
+  rule {
+    direction = "in"
+    protocol = "icmp"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
 # Create a server
 resource "hcloud_server" "k3s" {
   name        = "${var.env_name}-k3s"
   image       = "${var.node_image}"
   server_type = "${var.server_type}"
   ssh_keys = [ data.hcloud_ssh_key.ssh_key.id]
+
+  firewall_ids = [ hcloud_firewall.k3s-firewall.id ]
 
   user_data = "${data.template_file.cloud_init.rendered}"
 
